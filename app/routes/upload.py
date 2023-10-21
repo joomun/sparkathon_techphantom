0a3d62a87ab9930e2003bf1bdad1f84422ceaa8c
Joomun <joomunmuddathir@gmail.com>
2023-10-21 08:02:14 +0400

    Upload Excel On Uploads Folder-If folder not exist same created

--- app/routes/upload.py
+++ app/routes/upload.py
@@ -1,26 +1,17 @@
-from flask import Blueprint, render_template
-from flask import Flask, request, redirect, url_for, flash
+from flask import Blueprint, request, redirect, url_for, flash
 import pandas as pd
 from werkzeug.utils import secure_filename
 import os
 
-main = Blueprint('main', __name__)
-
-@main.route('/')
-def index():
-    return render_template('upload.html')
-
-
-app = Flask(__name__)
+upload_blueprint = Blueprint('upload', __name__)
 
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'xls', 'xlsx'}
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/upload', methods=['POST'])
+@upload_blueprint.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if 'file' not in request.files:
         flash('No file part')
@@ -30,8 +21,14 @@ def upload_file():
         flash('No selected file')
         return redirect(request.url)
     if file and allowed_file(file.filename):
+        # Ensure the uploads directory exists
+        if not os.path.exists(UPLOAD_FOLDER):
+            os.makedirs(UPLOAD_FOLDER)
+
         filename = secure_filename(file.filename)
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        filepath = os.path.join(UPLOAD_FOLDER, filename)
         file.save(filepath)
-        process_excel(filepath)
-        return redirect(url_for('upload_file', filename=filename))
+        # process_excel(filepath)
+        return redirect(url_for('main.index'))
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
